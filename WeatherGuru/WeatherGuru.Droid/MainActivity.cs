bc53e5d0e8a1443dacc74a26d45e1f5f34d402fd
rajshekhar.bhardwaj <rajshekhar.bhardwaj@NTPC02493>
2017-01-07 19:32:13 +0530

    Add project files.

---
+++ WeatherGuru/WeatherGuru.Droid/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using WeatherGuru;
+
+namespace WeatherGuru.Droid
+{
+	[Activity (Label = "WeatherGuru", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+            //Button button = FindViewById<Button>(Resource.Id.weatherBtn);
+            Button button = (Button)FindViewById(Resource.Id.weatherBtn);
+            button.Click += Button_Click;
+            //button.Click += (object sender, EventArgs e) =>
+            //{
+             //   Button_Click(sender, e);
+            //};
+            }
+       
+        private async void Button_Click(object sender,EventArgs e)
+        {
+            EditText zipCodeEntry = FindViewById<EditText>(Resource.Id.zipCodeEntry);
+
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+                {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                FindViewById<TextView>(Resource.Id.locationText).Text = weather.Title;
+                FindViewById<TextView>(Resource.Id.tempText).Text = weather.Temperature;
+                FindViewById<TextView>(Resource.Id.windText).Text = weather.Wind;
+                FindViewById<TextView>(Resource.Id.visibilityText).Text = weather.Visibility;
+                FindViewById<TextView>(Resource.Id.humidityText).Text = weather.Humidity;
+                FindViewById<TextView>(Resource.Id.sunriseText).Text = weather.Sunrise;
+                FindViewById<TextView>(Resource.Id.sunsetText).Text = weather.Sunset;
+
+            }
+        }
+
+    }
+}
+
+
---
+++ WeatherGuru/WeatherGuru/Core.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherGuru
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            string key = "f94b1691f4e2013a4040befc4250458f";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode+ "&appid=" + key + "&units=imperial";
+            // http://api.openweathermap.org/data/2.5/weather?zip=411057&appid=f94b1691f4e2013a4040befc4250458f&units=imperial
+            //Make sure developers running this sample replaced the API key
+            if (key != "f94b1691f4e2013a4040befc4250458f")
+            {
+                throw new ArgumentException("You must obtain an API key from openweathermap.org/appid and save it in the 'key' variable.");
+            }
+
+            dynamic results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+
+                return weather;
+            }
+            else
+                return null;
+        }
+    }
+}
